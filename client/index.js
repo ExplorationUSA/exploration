05a03f4d1080a6652da749a8808748cf3f6c7991
razananisathar <frazana.oa@gmail.com>
2020-12-16 12:56:40 +0530

    remove scss from webpack config

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import './assets/styles/app';
+import './assets/styles/app.scss';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     filename: '[name].bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.scss'],
+    extensions: ['.js', '.jsx'],
   },
   mode: process.env.NODE_ENV,
   devServer: {
