ae8b0f333b76349a03722cc5c0f05bf3f8bdafac
Anika <munikamustafiz89@gmail.com>
2020-12-19 18:12:10 -0500

    finish signup

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import {
   FormHelperText,
@@ -13,23 +13,63 @@ import {
   Flex,
 } from '@chakra-ui/react';
 
+
 const signup = () => {
   const [newUser, setNewUserField] = useState({
     userName: '',
     userEmail: '',
     userPassword: '',
+    confirmedUserPassword: '',
   });
+
+  const [error, setError] = useState({
+    verifyPassword: '',
+  });
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]); 
+
   const handleAllInputChange = (event) => {
     event.preventDefault();
     const { name, value } = event.target;
     setNewUserField({ ...newUser, [name]: value });
     console.log(event.target.value);
   };
+
+  const validate = () => {
+    const firstPasswordInput = newUser.userPassword;
+    const secondPasswordInput = newUser.confirmedUserPassword;
+    if (firstPasswordInput !== secondPasswordInput) {
+      console.log('in if statement')
+      setError({ ...error, verifyPassword: 'passwords do not match'});
+      return false;
+    }
+    console.log(error.verifyPassword);
+    return true;
+  }
+
   // backend function passed down in props that will take the currentUser as input;
   const handleNewUserSubmit = (event) => {
     event.preventDefault();
     console.log(newUser);
-  }; 
+
+    const errorStatus = validate();
+    console.log(errorStatus);
+
+    if (errorStatus) {
+      fetch('/api/signup', {
+        method: 'Post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      })
+      .then(() => console.log('fetch request with new user sent to server'))
+      .catch(() => console.log('An error occured in this fetch request to send new user'))
+    }
+}; 
+
   return (
     <LightMode>
       <Box>
@@ -37,12 +77,12 @@ const signup = () => {
           <Text colorScheme="blue" textAlign="center" letterSpacing="2px" mb={2}>Begin Your Exploration</Text>
           <Container marginBottom="1px solid silver" justifyContent="column">
             <form onSubmit={handleNewUserSubmit}>
-              <FormControl required>
+              <FormControl isRequired>
                 <FormLabel>Username:</FormLabel>
                 <Input onChange={handleAllInputChange} id="userName" name="userName" />
                 <FormHelperText fontSize="12px" id="email-helper-text">Set up a username</FormHelperText>
               </FormControl>
-              <FormControl required mt="10px">
+              <FormControl isRequired mt="10px">
                 <FormLabel>User Email:</FormLabel>
                 <Input onChange={handleAllInputChange} id="userEmail" name="userEmail" />
                 <FormHelperText fontSize="12px" id="email-helper-text">Your user account email address</FormHelperText>
@@ -50,9 +90,16 @@ const signup = () => {
               <FormControl mt="10px" required>
                 <FormLabel>Password:</FormLabel>
                 <Input onChange={handleAllInputChange} id="userPassword" type="password" name="userPassword" />
+                {error.verifyPassword && <Text id="mismatchedPassword" fontSize="8px">{error.verifyPassword}</Text>}
                 <FormHelperText fontSize="12px" id="password-helper-text">Set up a password</FormHelperText>
               </FormControl>
-              <Button colorScheme="cyan" color="white" ml="60px" mt={4} type="submit">Submit</Button>
+              <FormControl mt="10px" required>
+                <FormLabel>Confirm Password:</FormLabel>
+                <Input onChange={handleAllInputChange} id="confirmedUserPassword" type="password" name="confirmedUserPassword" />
+                {error.verifyPassword && <Text id="mismatchedPassword" fontSize="8px">{error.verifyPassword}</Text>}
+                <FormHelperText fontSize="12px" id="password-helper-text">Set up a password</FormHelperText>
+              </FormControl>
+              <Button colorScheme="cyan" color="white" ml="60px" mt={4} type="submit">Let us explore</Button>
             </form>
           </Container>
           <Container>
