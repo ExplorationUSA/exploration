67fb6f2090773789b7f4141cfb1b0a1dd27704c9
nbargers <nbargers@gmail.com>
2020-12-25 14:45:18 -0500

    trip controller edit and delete

--- server/controllers/tripController.js
+++ server/controllers/tripController.js
@@ -8,11 +8,35 @@ tripController.createTrip = async (req, res, next) => {
       title, destination, start_date, end_date,
     } = req.body;
     const member_id = req.session.passport.user;
+    
+    if(title === undefined || destination === undefined || start_date === undefined || end_date === undefined) {
+      next({
+        log: `tripController.createTrip: Request parameters are empty'`,
+        status: 406,
+        message: {
+          err: 'Request parameters are empty',
+        },
+      })
+    }
+
+    const duplicateQuery = 'SELECT FROM trip WHERE member_id = $1 AND title = $2';
+    const duplicateTrip = Pool.query(duplicateQuery, [member_id, title]);
+
+    if(duplicateTrip.rowCount !== 0){
+      next({
+        log: 'tripController.createTrip: Trip with this title already exists',
+        status: 409,
+        message: {
+          err: 'Trip with this title already exists',
+        },
+      })
+    }
 
     const query = 'INSERT INTO trip (title, destination, start_date, end_date, member_id) VALUES ($1, $2 , $3, $4, $5)';
     const trip = await Pool.query(query, [title, destination, start_date, end_date, member_id]);
-    console.log(trip);
+   
     next();
+
   } catch (error) {
     next({
       log: `tripController.createTrip: ${error}`,
@@ -33,6 +57,7 @@ tripController.getTrips = async (req, res, next) => {
     res.locals.trips = trips.rows;
 
     next();
+
   } catch (error) {
     next({
       log: `tripController.getTrips: ${error}`,
@@ -61,13 +86,45 @@ tripController.editTrip = async (req, res, next) => {
       })
     }
     
-    const query = "UPDATE trips SET destination = $1, start_date = $2, end_date = $3 WHERE title = $4"
-    const trip = Pool.query(query, [destination, start_date, end_date, title])
-    console.log(trip)
-    next()
+    const query = "UPDATE trips SET destination = $1, start_date = $2, end_date = $3 WHERE title = $4";
+    const trip = Pool.query(query, [destination, start_date, end_date, title]);
+   
+    next();
+
   } catch (error) {
     next({
       log: `tripController.editTrip: ${error}`,
+      status: 500,
+      message: {
+        err: 'Internal server error',
+      },
+    })
+  }
+};
+
+tripController.deleteTrip = (req, res, next) => {
+  try {
+
+    const title = req.body;
+    const member_id = req.session.passport.user;
+
+    if(title === undefined){
+      next({
+        log: `tripController.deleteTrip: Request parameters are empty'`,
+        status: 406,
+        message: {
+          err: 'Request parameters are empty',
+        },
+      })
+    }
+
+    const query = 'DELETE FROM trip WHERE title = $1 AND member_id = $2';
+    const tripDelete = Pool.query(query, [title, member_id])
+    next();
+    
+  } catch (error) {
+    next({
+      log: `tripController.deleteTrip: ${error}`,
       status: 500,
       message: {
         err: 'Internal server error',
--- server/routes/trips.js
+++ server/routes/trips.js
@@ -5,15 +5,19 @@ const authController = require('../controllers/authController');
 const tripController = require('../controllers/tripController');
 
 route.post('/create', authController.isAuthenticated, tripController.createTrip, (req, res) => {
-  res.status(200).json({message: 'trip created'});
+  res.status(200).json({message: 'Trip Created'});
 });
 
 route.get('/retrieve', authController.isAuthenticated, tripController.getTrips, (req, res) => {
   res.status(200).json({trips: res.locals.trips});
-})
+});
 
-route.post('/edit', authController.isAuthenticated, tripController.editTrip, (req, res) =>{
-  res.status(200);
-})
+route.post('/edit', authController.isAuthenticated, tripController.editTrip, (req, res) => {
+  res.status(200).json({message: 'Trip Edited'});
+});
+
+route.delete('/delete', authController.isAuthenticated, tripController.deleteTrip, (req, res) => {
+  res.status(200).json({message: 'Trip Deleted'});
+});
 
 module.exports = route;
