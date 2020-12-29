8b854d8d4766538c2ff4d46347f1d90bd9d910b7
razananisathar <frazana.oa@gmail.com>
2020-12-29 11:34:52 +0530

    add user object to signup and signin routes

--- server/passport.config.js
+++ server/passport.config.js
@@ -3,27 +3,30 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const Pool = require('./model/database');
 
-passport.use('local', new LocalStrategy((async (username, password, done) => {
-  try {
-    const query = 'SELECT * FROM member WHERE username = $1';
-    const member = await Pool.query(query, [username]);
-    if (member.rows.length === 0) {
-      done(null, false, { message: 'Incorrect username.' });
-    } else {
-      bcrypt.compare(password, member.rows[0].password, (err, isMatch) => {
-        if (err) {
-          done(null, false, { message: `Decrypt error: ${err}` });
-        } 
-        if (isMatch) {
-          const { id, username, password } = member.rows[0];
-          done(null, { id, username, password });
-        } else done(null, false, { message: 'Incorrect password.' });
-      });
+passport.use(
+  'local',
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const query = 'SELECT * FROM member WHERE username = $1';
+      const member = await Pool.query(query, [username]);
+      if (member.rows.length === 0) {
+        done(null, false, { message: 'Incorrect username.' });
+      } else {
+        bcrypt.compare(password, member.rows[0].password, (err, isMatch) => {
+          if (err) {
+            done(null, false, { message: `Decrypt error: ${err}` });
+          }
+          if (isMatch) {
+            const { id, username } = member.rows[0];
+            done(null, { id, username });
+          } else done(null, false, { message: 'Incorrect password.' });
+        });
+      }
+    } catch (err) {
+      return done(null, false, { message: `${err}` });
     }
-  } catch (err) {
-    return done(null, false, { message: `${err}` });
-  }
-})));
+  })
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
