c50c4ca7f836c95badfed5ac0b52a391d40f23f9
razananisathar <frazana.oa@gmail.com>
2020-12-29 17:59:35 +0530

    add support for page refresh and skip proxy for browser request

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use('/api/trips', routeTrips);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist/')));
 
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
