8a253300d043c136cb94866689ae2fed65aeaa8e
Nathan Bargers <nbargers@gmail.com>
2021-01-02 17:44:14 -0500

    database variables updates

--- server/model/database.sql
+++ server/model/database.sql
@@ -7,18 +7,27 @@ email VARCHAR(255) NOT NULL UNIQUE
 
 CREATE TABLE trip(
 id SERIAL PRIMARY KEY,
-title VARCHAR(255) NOT NULL, 
-destination VARCHAR(255) NOT NULL,
+title VARCHAR(1000) NOT NULL, 
+destination VARCHAR(1000) NOT NULL,
 start_date DATE NOT NULL,
 end_date DATE NOT NULL,
 member_id INT NOT NULL, 
+place_id VARCHAR(1000),
+locationphotos [],
+dates_known  VARCHAR(1000),
 FOREIGN KEY (member_id) REFERENCES member(id)
 );
 
 CREATE TABLE activity(
 id SERIAL PRIMARY KEY,
-title VARCHAR(255) NOT NULL, 
-location VARCHAR(255) NOT NULL,
+title VARCHAR(1000) NOT NULL, 
+location VARCHAR(1000) NOT NULL,
 trip_id INT NOT NULL, 
+image_url VARCHAR(1000),
+url VARCHAR(1000),
+latitude DECIMAL,
+longitude DECIMAL,
+rating DECIMAL,
+review_count INT
 FOREIGN KEY (trip_id) REFERENCES trip(id)
 );
